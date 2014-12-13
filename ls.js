0dfdcb29029eb3ecc2067ad85d00d6640bf224a7
Vladimir Grinenko <i@tadatuta.com>
2014-12-13 23:00:59 +0300

    add some TODO notes

--- ls.js
+++ ls.js
@@ -4,12 +4,16 @@ var path = require('path'),
     walk = require('bem-walk'),
     glob = require('glob'),
     args = process.argv,
+    // TODO: support all args with index > 1 as levels array
     cliLevels = args[2] && args[2].split(',') || ['*.blocks', '**/*.blocks'],
     levels = [];
 
 cliLevels.forEach(function(level) {
     levels = levels.concat(glob.sync(level));
 });
+
+// TODO: try this way
+// walk(levels).pipe(process.stdout);
 
 walk(levels).on('data', function(entity) {
     console.log(entity.path);
